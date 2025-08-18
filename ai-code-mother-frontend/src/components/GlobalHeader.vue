98195fd66eb728eb123c21aed6fce47ff5a7d373
agx <1296533696@qq.com>
2025-08-18 21:02:14 +0800

    前端修复对话管理页面显示小问题

--- ai-code-mother-frontend/src/components/GlobalHeader.vue
+++ ai-code-mother-frontend/src/components/GlobalHeader.vue
@@ -83,6 +83,11 @@ const originItems = [
     title: '应用管理',
   },
   {
+    key: '/admin/chatManage',
+    label: '对话管理',
+    title: '对话管理',
+  },
+  {
     key: 'others',
     label: h('a', { href: 'https://github.com/AoGuangXiao/ai-code-mother', target: '_blank' }, 'github'),
     title: 'github',
