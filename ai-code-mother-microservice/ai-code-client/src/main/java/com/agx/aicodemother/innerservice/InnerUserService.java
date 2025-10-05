8c5e2fd5505663c3f6dedd4d9fd4bd100a222a64
agx <1296533696@qq.com>
2025-10-05 15:05:33 +0800

    微服务改造 - 服务接口模块

---
+++ ai-code-mother-microservice/ai-code-client/src/main/java/com/agx/aicodemother/innerservice/InnerUserService.java
@@ -0,0 +1,35 @@
+package com.agx.aicodemother.innerservice;
+
+import com.agx.aicodemother.exception.BusinessException;
+import com.agx.aicodemother.exception.ErrorCode;
+import com.agx.aicodemother.model.entity.User;
+import com.agx.aicodemother.model.vo.UserVO;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import static com.agx.aicodemother.constant.UserConstant.USER_LOGIN_STATE;
+
+/**
+ * 内部使用的用户服务
+ */
+public interface InnerUserService {
+
+    List<User> listByIds(Collection<? extends Serializable> ids);
+
+    User getById(Serializable id);
+
+    UserVO getUserVO(User user);
+
+    // 静态方法，避免跨服务调用
+    static User getLoginUser(HttpServletRequest request) {
+        Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
+        User currentUser = (User) userObj;
+        if (currentUser == null || currentUser.getId() == null) {
+            throw new BusinessException(ErrorCode.NOT_LOGIN_ERROR);
+        }
+        return currentUser;
+    }
+}
